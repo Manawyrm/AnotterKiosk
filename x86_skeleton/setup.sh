0d0c64a579e4be96af8dbabb9662ca9fcc7f82a2
Manawyrm <git@tbspace.de>
2024-03-07 17:55:59 +0100

    x86: use linux kernel from bookworm-backports

--- x86_skeleton/setup.sh
+++ x86_skeleton/setup.sh
@@ -3,6 +3,7 @@
 apt update
 # make sure we have all updates installed (from the -updates and -security repos)
 APT_LISTCHANGES_FRONTEND=none DEBIAN_FRONTEND=noninteractive apt -o Dpkg::Options::="--force-confold" -f -y dist-upgrade
+APT_LISTCHANGES_FRONTEND=none DEBIAN_FRONTEND=noninteractive apt install -f -y -t bookworm-backports linux-image-amd64
 # to remove old kernel versions
 apt --purge autoremove
 apt install -y polkitd locales zstd dhcpcd wpasupplicant
